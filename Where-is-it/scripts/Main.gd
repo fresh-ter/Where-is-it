bb6b5db7df677d59d2384dc291c0e6670266219b
Terminator <fresh-ter@yandex.com>
2022-11-19 12:14:26 +0400

    Add click areas

--- Where-is-it/scripts/Main.gd
+++ Where-is-it/scripts/Main.gd
@@ -1,13 +1,14 @@
 extends Node
 
-onready var building = preload("res://example_building/Building.tscn")
+#onready var building = preload("res://example_building/Building.tscn")
 var speed = 100
 var speed_scale = 100
 
 func _ready():
-	var a = building.instance()
-	$Map.add_child(a)
-	a.global_transform = $Map.global_transform
+#	var a = building.instance()
+#	$Map.add_child(a)
+#	a.global_transform = $Map.global_transform
+	test()
 	pass # Replace with function body.
 	
 #	$Map.apply_scale(Vector2(0.1,0.1))
@@ -15,22 +16,37 @@ func _ready():
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
 #		print($Node2D.position)
-		$Map.translate(Vector2(5,0)*speed*delta)
+		$Building.translate(Vector2(5,0)*speed*delta)
 	if Input.is_action_pressed("ui_right"):
-		$Map.translate(Vector2(-5,0)*speed*delta)
+		$Building.translate(Vector2(-5,0)*speed*delta)
 	if Input.is_action_pressed("ui_up"):
-		$Map.translate(Vector2(0,5)*speed*delta)
+		$Building.translate(Vector2(0,5)*speed*delta)
 	if Input.is_action_pressed("ui_down"):
-		$Map.translate(Vector2(0,-5)*speed*delta)
+		$Building.translate(Vector2(0,-5)*speed*delta)
 	
 	if Input.is_action_just_pressed("map_zoom_in"):
-		$Map.apply_scale(Vector2(1,1)*speed_scale*delta)
+		$Building.apply_scale(Vector2(1,1)*speed_scale*delta)
 	if Input.is_action_just_pressed("map_zoom_out"):
-		$Map.apply_scale(Vector2(0.5,0.5)*speed_scale*delta)
+		$Building.apply_scale(Vector2(0.5,0.5)*speed_scale*delta)
+
+
+func test():
+#	print($Building.get_child_count())
+#	print($Building.get_children())
+	
+	for level in $Building.get_children():
+		for area in level.get_node("Areas").get_children():
+			print(area)
+			area.get_node("Area2D").connect('input_event', self, 'test_slot', [area.name])
+
+func test_slot(viewport: Node, event: InputEvent, shape_idx: int, name: String):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			print("Test: " + name)
 
 
 
 func _on_VSlider_value_changed(value):
 	print(value)
-	$Map.scale = Vector2(value, value)
+	$Building.scale = Vector2(value, value)
 	pass # Replace with function body.
